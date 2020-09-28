e4bedf59c38c8e92b65d62f8c4ed7d5dc5ba4c00
Zavesky, Eric (ez2685) <ez2685@att.com>
2020-09-28 02:28:35 -0500

    updates on data, active learning
    - update labels from campaign
    - update notebook A for dependencies
    - update notebook D for active learning improvements
    - update zip asset script

--- scripts/22_zip_assets.sh
+++ scripts/22_zip_assets.sh
@@ -9,7 +9,8 @@ mkdir -p $ROOT_DIR/notebooks
 
 echo "Please make sure you've downloaded all of your relevant contentai data first... (see 00_make_batch.sh)"
 
-rsync -aP --delete --exclude='.*' --exclude="*auth*.json" --exclude='*.pkl*' --exclude=packages $ROOT_DIR/cmlp/work/. $ROOT_DIR/notebooks/.
+#rsync -aP --delete --exclude='.*' --exclude="*auth*.json" --exclude=packages $ROOT_DIR/cmlp/work/. $ROOT_DIR/notebooks/.
+rsync -aP --delete --exclude='.*' --exclude="*auth*.json" --exclude='models/*' --exclude=packages $ROOT_DIR/cmlp/work/. $ROOT_DIR/notebooks/.
 cd $ROOT_DIR/notebooks
 
 # grab only the python notebooks and content under 'assets'
